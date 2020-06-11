feffee07196bfc6233a2fa3575713883836dddd6
Hideyuki Yamashita <yamashita.hideyuki@ntt-tx.co.jp>
2020-06-11 11:54:29 +0900

    Initial Commit

---
+++ src/drivers/pipe/.librte_pmd_spp_pipe.a.cmd
@@ -0,0 +1,1 @@
+cmd_librte_pmd_spp_pipe.a = ar crDs librte_pmd_spp_pipe.a rte_spp_pipe.o 
---
+++ src/drivers/pipe/rte_spp_pipe.o.pmd.c
@@ -0,0 +1,1 @@
+const char spp_pipe_pmd_info[] __attribute__((used)) = "PMD_INFO_STRING= {\"name\" : \"spp_pipe\", \"params\" : \"rx=<rx_ring> tx=<tx_ring>\", \"pci_ids\" : []}";
